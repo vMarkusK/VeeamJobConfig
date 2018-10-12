e8caa57342b6d7a6acfdcad10dbcfae16c18aac7
mycloudrevolution <markus.kraus@gmail.com>
2018-10-12 11:16:49 +0200

    initial version of Set-VBRJobOptionsFromRef

---
+++ Playground/Set-VBRJobOptionsFromRef.ps1
@@ -0,0 +1,31 @@
+function Set-VBRJobOptionsFromRef {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [ValidateNotNullorEmpty()]
+            [Veeam.Backup.Core.CBackupJob]$BackupJob,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$false)]
+        [ValidateNotNullorEmpty()]
+            [Veeam.Backup.Core.CBackupJob]$ReferenceBackupJob
+    )
+    
+    process {
+        try {
+            $JobRefOptions = $ReferenceBackupJob.GetOptions()
+        }
+        catch {
+            throw "Get Options Failed for '$($ReferenceBackupJob.Name)'"    
+        }
+        
+        
+        try {
+            Set-VBRJobOptions -Job $BackupJob -Options $JobRefOptions
+        }
+        catch {
+            throw "Set Options Failed for '$($BackupJob.Name)'"
+            
+        }
+
+    }
+    
+}
