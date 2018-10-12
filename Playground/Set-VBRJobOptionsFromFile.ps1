adff4a2dbeaf115be7f23f4fbf19c92f87f4dc33
mycloudrevolution <markus.kraus@gmail.com>
2018-10-12 11:27:31 +0200

    initial Version of Set-VBRJobOptionsFromFile

---
+++ Playground/Set-VBRJobOptionsFromFile.ps1
@@ -0,0 +1,69 @@
+function Set-VBRJobOptionsFromFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [ValidateNotNullorEmpty()]
+            [Veeam.Backup.Core.CBackupJob]$BackupJob,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$false)]
+        [ValidateNotNullorEmpty()]
+            [String]$ReferenceFile
+    )
+
+    begin {
+        if (Test-Path $ReferenceFile) {
+            $JsonConfig = Get-Content -Raw -Path $ConfigFile | ConvertFrom-Json
+            $RefBackupStorageOptions = $JsonConfig.BackupStorageOptions
+            #$RefBackupTargetOptions  = $JsonConfig.BackupTargetOptions
+            $RefJobOptions = $JsonConfig.JobOptions
+            $RefNotificationOptions = $JsonConfig.NotificationOptions
+        }
+        else {
+            Throw "Reference File not Found"
+        }
+
+    }
+
+    process {
+        # Get the actual Job Options 
+        $JobOptions = $BackupJob.GetOptions()
+    
+        # Set the reference Job Options
+        ## BackupStorageOptions
+        try {
+            $RefBackupStorageOptions.PSObject.Properties | ForEach-Object {
+            $JobOptions.BackupStorageOptions.$($_.Name) = $($_.Value)    
+            }
+        
+        }
+        catch {
+            Throw "Section 'BackupStorageOptions' Failed!"
+        }
+        
+        ## JobOptions
+        try {
+            $RefJobOptions.PSObject.Properties | ForEach-Object {
+            $JobOptions.JobOptions.$($_.Name) = $($_.Value)    
+            }
+        
+        }
+        catch {
+            Throw "Section 'JobOptions' Failed!"
+        }
+        
+        ## NotificationOptions
+        try {
+            $RefNotificationOptions.PSObject.Properties | ForEach-Object {
+            $JobOptions.NotificationOptions.$($_.Name) = $($_.Value)    
+        
+            }
+        }
+        catch {
+            Throw "Section 'NotificationOptions' Failed!"
+        }
+        
+        # Update Job Options
+        Set-VBRJobOptions $VeeamJob $JobOptions
+        
+    }
+    
+}
